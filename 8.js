16c8b470829ab4b456a0e2caeddf6ed697c12e23
truongnguyenhitachi <truong.truong@hitachivantara.com>
2023-06-12 20:54:02 +0700

    leetcode day2

---
+++ 8.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function(s) {
+    const len = s.length;
+   let result = 0;
+   let sign = 1;
+   for(let i=0; i<len; i++){
+    //    step 1:
+       if(s[i]===' ') continue;
+    //    step 2:
+       if(s[i] === '+' || s[i]==='-'){
+           sign = s[i] === '-' ? -1 : 1;
+           if(!/[0-9]/.test(s[i+1])) return 0;
+       }
+    //    step 3:
+        if(/[0-9]/.test(s[i])){
+            result = result*10 + (+s[i]);
+            if(!/[0-9]/.test(s[i+1])) break;
+        }
+    // break point:
+        if(/[a-zA-Z.]/.test(s[i])){
+            return 0;
+        }
+   }   
+   return sign === -1 ? Math.max(-(2 ** 31), -result) : Math.min(2 ** 31 - 1, result);
+};
